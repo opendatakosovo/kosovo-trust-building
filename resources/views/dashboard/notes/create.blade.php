8beea0c04a77167982b49623e67a907950d4ae62
the3reze <ereze.ahmetgjekaj@gmail.com>
2021-03-03 14:03:19 +0100

    .

--- resources/views/dashboard/notes/create.blade.php
+++ resources/views/dashboard/notes/create.blade.php
@@ -61,13 +61,23 @@
 
 <script src="{{ ('https://cdn.tiny.cloud/1/s1cvah5fcucfql4r7y91sdyqegend95iv617ra8qvn4ma36r/tinymce/5/tinymce.min.js') }}"></script>
 <script>
-    tinymce.init({
-      selector: 'textarea',
-      plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-      toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-      toolbar_mode: 'floating',
-      tinycomments_mode: 'embedded',
-      tinycomments_author: 'Author name',
-   });
+  tinymce.init({
+  selector: 'textarea',
+  width: 600,
+  height: 300,
+  plugins: [
+    'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+    'table emoticons template paste help'
+  ],
+  toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+    'forecolor backcolor emoticons | help',
+  menu: {
+    favs: {title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons'}
+  },
+  menubar: 'favs file edit view insert format tools table help',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+});
   </script>
 @endsection

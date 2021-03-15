d901b885340b6ff3500af2888eb6b52ebae0881d
Tringa <tringa.prekorogjaa@opendatakosvo.org>
2021-03-15 09:44:20 +0100

    Add form for do you want to add something

--- resources/views/frontend/includes/banner.blade.php
+++ resources/views/frontend/includes/banner.blade.php
@@ -12,11 +12,42 @@
                                 </div>                  
                                <div class="col-md-6">
                                     <h2>Do you want to add something?</h2>
-                                    <a class="button">Do it here!</a>
+                                    <a class="button" data-toggle="modal" data-target="#myModal">Do it here!</a>
                                </div>                     
                             </div>
                         </div><!-- /.col-md-12 -->
                     </div><!-- /.row -->
                 </div><!-- /.container -->
+                <!-- <div class="container"> -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="modal fade" id="myModal">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            <form class="form" action="">
+                                                <div class="personal-info">
+                                                    <div class="name">
+                                                        <label for="nameInput">Full Name</label>
+                                                        <input type="text" placeholder="Full name" id="nameInput" name="name">
+                                                    </div>
+                                                    <div class="email">
+                                                        <label for="emailInput">Email address</label>
+                                                        <input type="text" placeholder="E-mail" id="emailInput" name="email">
+                                                    </div>      
+                                                </div>
+                                                <label for="organizationInput">Organization</label>
+                                                <input type="text" placeholder="Organization" id="organizationInput" name="organization">
+                                                <label for="storyInput" id="story">You story</label>
+                                                <textarea name="story" cols="24" rows="3"></textarea>
+                                                <input class="button button--modal" data-dismiss="modal" value="Share your story">
+                                            </form> 
+                                        </div>    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
             </div>
         </div>
